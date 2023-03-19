19f30fd6c5127867f8dd715927f68276661b9651
MOHIT <84301563+mohitjangra75@users.noreply.github.com>
2023-03-19 12:32:30 +0530

    Delete docker-entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-echo "Applying migrations..."
-python manage.py migrate --noinput
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-echo "Starting server..."
-gunicorn djangoblog.wsgi
-exec "$@"
